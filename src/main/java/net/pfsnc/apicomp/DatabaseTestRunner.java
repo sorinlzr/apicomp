2164f29df5f3682d944b21948ded7c3c81671cf6
sorin.lzr <soso.laz@gmail.com>
2025-01-06 09:46:03 +0100

    add DatabaseTestRunner and property in application.yaml to execute the runner conditionally

---
+++ src/main/java/net/pfsnc/apicomp/DatabaseTestRunner.java
@@ -0,0 +1,28 @@
+package net.pfsnc.apicomp;
+
+import net.pfsnc.apicomp.fh.model.Teacher;
+import net.pfsnc.apicomp.fh.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConditionalOnExpression("${testRunner.database:true}")
+public class DatabaseTestRunner implements CommandLineRunner {
+
+    private final TeacherService teacherService;
+
+    @Autowired
+    public DatabaseTestRunner(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @Override
+    public void run(String... args) {
+        List<Teacher> teachers = teacherService.findAll();
+        teachers.stream().limit(50).forEach(teacher -> System.out.println(teacher.getName()));
+    }
+}
